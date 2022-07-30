bba702cff0521d614bc5ac78ab37ba9624660a5d
shreymo12 <69166349+shreymo12@users.noreply.github.com>
2022-07-30 16:13:23 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# C96
+# CleanWater2
---
+++ screens/DonateScreen.js
@@ -0,0 +1,80 @@
+import React, {Components} from 'react';
+import {View, StyleSheet, TouchableOpacity, TextInput, Text} from 'react-native';
+
+import HomeScreen from './HomeScreen';
+
+
+export default class DonateScreen extends Components{
+  goToHomeScreen = ()=>{
+    <Text style = {styles.buttonText}>
+       Go Home
+
+    </Text>
+    this.props.navigation.navigate('HomeScreen')
+  }
+  constructor(){
+    super();
+    this.state ={
+      userId: "",
+      amountDonated: ""
+    };
+  }
+  donatedAmount = (userId, amountDonated) => { 
+    firebase 
+    .createUserId (userID, amountDonated) 
+    .then(()=>{ db.collection("Donations").add({ 
+      Name: this.state.userId, 
+      amount: this.state.amountDonated
+       }); 
+       }) 
+
+  
+  }
+    render(){
+        return(
+
+           <View>
+            <TextInput
+              placeholder= "User Id"
+              placeholderTextColor = "gray"
+              keyboardType = "user-id"
+              onChangeText = {text=>{
+                this.setState({
+                  userId:text
+                })
+              }}
+            />
+            <TextInput
+              placeholder = "$ Donated"
+              placeholderTextColor = "gray"
+              keyboardType = "amount"
+              onChangeText = {number =>{
+                this.setState ({
+                  amountDonated : number
+                })
+              }}
+            />
+
+            <TouchableOpacity
+            style = {[styles.button,{backgroundColor: "yellow"}]}
+            onPress = {this.goToHomeScreen}>
+            </TouchableOpacity>
+           </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+  button:{
+    justifyContent: 'center', 
+    alignSelf: 'center',
+    borderWidth : 2, 
+    marginTop : 50, 
+    with: 200,
+    height: 50
+  },
+  buttonText: {
+    textAlign: 'center',
+    color : "gray"
+  }
+})
